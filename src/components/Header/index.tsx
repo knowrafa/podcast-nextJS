70919337c0e04df10f1d0761fa35854e980ecb1f
rafael <noprafa@gmail.com>
2021-07-28 10:03:20 -0300

    Criando projeto em Next

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import format from 'date-fns/format'
+import ptBR from 'date-fns/locale/pt-BR'
+import styles from './styles.module.scss'
+
+export default function Header(){
+
+    // const currentDate = new Date().toLocaleDateString();
+
+    const currentDate = format(new Date(), 'EEEEEE, d, MMMM', {
+        locale: ptBR,
+    });
+
+    return ( 
+        <header className={styles.headerContainer}>
+            <img src="/logo.svg" alt="Podcast"></img>
+            <p>O melhor para você ouvir</p>
+            <span>{currentDate}</span>
+        </header>
+    );
+
+}
